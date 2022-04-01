f4c3f0147dca1316e4603eb66d3f1d081f9fb0de
Arun3sh <m.aruneshwar@gmail.com>
2022-04-01 23:55:08 +0530

    hall-booking initiated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import Home from './components/Home';
+import BookRoom from './components/BookRoom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/book-room/:id">
+					<BookRoom />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
