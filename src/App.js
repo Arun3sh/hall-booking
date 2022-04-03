6dea17787de06d8b8972057349205a4463ff6baa
Arun3sh <m.aruneshwar@gmail.com>
2022-04-03 23:51:18 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import Home from './components/Home';
+import BookRoomForm from './components/BookRoomForm';
+import { toast } from 'react-toastify';
+import Createroom from './components/Createroom';
 import BookRoom from './components/BookRoom';
+
+toast.configure();
 
 function App() {
 	return (
@@ -12,8 +17,14 @@ function App() {
 				<Route exact path="/">
 					<Home />
 				</Route>
-				<Route path="/book-room/:id">
+				<Route path="/book_room">
 					<BookRoom />
+				</Route>
+				<Route path="/book-room/:id">
+					<BookRoomForm />
+				</Route>
+				<Route path="/create-new-room">
+					<Createroom />
 				</Route>
 			</Switch>
 		</div>
