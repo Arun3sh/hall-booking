fed51146074aee84ab71377ec373f9228340ea2a
Arun3sh <m.aruneshwar@gmail.com>
2022-04-03 23:50:59 +0530

    additional

---
+++ src/components/roomDataFunc.js
@@ -0,0 +1,64 @@
+export function roomDataFunc() {
+	return [
+		{
+			id: 101,
+			amentities: [{ chair: 2, table: 1, Wifi: 'yes', tv: 'yes' }],
+			seats: 2,
+			price: 100,
+		},
+		{
+			id: 102,
+			amentities: [{ chair: 2, table: 1, Wifi: 'yes', tv: 'no' }],
+			seats: 2,
+			price: 80,
+		},
+		{
+			id: 103,
+			amentities: [{ chair: 2, table: 1, Wifi: 'no', tv: 'no' }],
+			seats: 2,
+			price: 60,
+		},
+		{
+			id: 104,
+			amentities: [{ chair: 10, table: 1, Wifi: 'yes', tv: 'yes' }],
+			seats: 10,
+			price: 500,
+		},
+		{
+			id: 105,
+			amentities: [{ chair: 5, table: 1, Wifi: 'yes', tv: 'yes' }],
+			seats: 5,
+			price: 250,
+		},
+		{
+			id: 106,
+			amentities: [{ chair: 25, table: 5, Wifi: 'yes', tv: 'yes' }],
+			seats: 25,
+			price: 1250,
+		},
+		{
+			id: 107,
+			amentities: [{ chair: 10, table: 3, Wifi: 'yes', tv: 'no' }],
+			seats: 10,
+			price: 600,
+		},
+		{
+			id: 108,
+			amentities: [{ chair: 3, table: 1, Wifi: 'no', tv: 'no' }],
+			seats: 3,
+			price: 90,
+		},
+		{
+			id: 109,
+			amentities: [{ chair: 10, table: 2, Wifi: 'yes', tv: 'yes' }],
+			seats: 10,
+			price: 600,
+		},
+		{
+			id: 110,
+			amentities: [{ chair: 5, table: 1, Wifi: 'yes', tv: 'yes' }],
+			seats: 5,
+			price: 250,
+		},
+	];
+}
