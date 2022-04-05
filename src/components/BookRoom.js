ada53a79cb7286f9a9219b3d58979d531fdec850
Arun3sh <m.aruneshwar@gmail.com>
2022-04-05 23:54:09 +0530

    api to get room data

--- src/components/BookRoom.js
+++ src/components/BookRoom.js
@@ -1,29 +1,38 @@
 import { roomDataFunc } from './roomDataFunc';
 import { Button } from '@mui/material';
 import { useHistory } from 'react-router-dom';
+import { API } from './global';
+import { useEffect, useState } from 'react';
 
 function BookRoom() {
 	const history = useHistory();
-	const roomData = roomDataFunc();
+	const [roomData, setRoomData] = useState([]);
+	const getroomData = () => {
+		fetch(`${API}/rooms`, {
+			method: 'GET',
+		})
+			.then((data) => data.json())
+			.then((data) => setRoomData(data));
+	};
+	useEffect(getroomData, []);
 
 	return (
 		<div className="bookroom-wrapper container-sm">
-			{roomData.map(({ id, amentities, seats, price }, index) => (
+			<h5>All Rooms</h5>
+			{roomData.map(({ _id, room_id, chair, table, wifi, tv, seats, price }, index) => (
 				<div key={index} className="container-home">
 					<h3>
-						Room ID: <span className="room-id">{id}</span>
+						Room ID: <span className="room-id">{room_id}</span>
 					</h3>
 					<p>Seats: {seats}</p>
 					<p>Price per hour - {price} </p>
 					<h5>Amentities</h5>
 					<p>
-						{amentities.map(({ chair, table, Wifi, tv }, index) => (
-							<span key={index}>
-								Chair - {chair}, Table - {table}, Wifi - {Wifi}, TV - {tv}{' '}
-							</span>
-						))}
+						<span>
+							Chair - {chair}, Table - {table}, Wifi - {wifi}, TV - {tv}{' '}
+						</span>
 					</p>
-					<Button variant="outlined" onClick={() => history.push(`/book-room/${id}`)}>
+					<Button variant="outlined" onClick={() => history.push(`/book-room/${_id}`)}>
 						Book Room
 					</Button>
 				</div>
